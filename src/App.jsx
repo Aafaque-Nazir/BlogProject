f28f3c4e3fe62fe17cec027814635523906413eb
Aafaque Nazir <aafaquenazir@gmail.com>
2025-03-19 08:11:37 +0530

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-10">Hello, Vite + React!</h1>
+      <p className="text-center mt-5">Edit <code>App.jsx</code> and save to test HMR updates.</p>
+      
+      
+    </>
+  )
+}
+
+export default App

d6017b3d98cb194790d8493ed52a9d548010ef44
Aafaque Nazir <aafaquenazir@gmail.com>
2025-04-01 11:05:16 +0530

    `Update favicon and metadata, change logo and copyright year in Footer component`

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from '../image/logo.png'
 
-function Logo({width = '100px'}) {
+function Logo({width = '53px'}) {
   return (
-    <div>Logo</div>
+    <div>
+      <img src={logo} alt="Logo" style={{width}} ></img>
+    </div>
   )
 }
 
